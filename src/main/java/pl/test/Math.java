b23ee3ef58cd12bcde433037db40ecc7493153f0
pawelpawluczuk <pawelpawluczuk@gmail.com>
2017-01-05 19:51:49 +0100

    2

---
+++ src/main/java/pl/test/Math.java
@@ -0,0 +1,12 @@
+package pl.test;
+
+import java.util.List;
+
+/**
+ * Created by Pawel Pawluczuk on 05/01/2017.
+ */
+public class Math {
+    public static List<Factors> getFactors(int number){
+        return null;
+    }
+}
