91c85957d144b28e84b0700b1092b9bc737b6385
mondzik <mondzik777@gmail.com>
2017-01-05 20:15:23 +0100

    pierwsza proba

--- src/main/java/pl/test/Math.java
+++ src/main/java/pl/test/Math.java
@@ -1,12 +1,38 @@
 package pl.test;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
  * Created by Pawel Pawluczuk on 05/01/2017.
  */
+//class Factors
+//{
+//    private int x,y;
+//    public int getX() { return x;}
+//    public int getY() { return y;}
+//    public void setX(int x) { this.x=x;}
+//    public void setY(int y) { this.y=y;}
+//
+//    public Factors() {}
+//    public Factors(int x,int y) { this.x=x; this.y=y;}
+//}
+
+
 public class Math {
     public static List<Factors> getFactors(int number){
-        return null;
+        int testNumber=1;
+        List<Factors> factors=new ArrayList<Factors>();
+        while(testNumber<=number)
+        {
+
+            if (testNumber%number==0){
+                factors.add(new Factors(testNumber,number/testNumber));
+            }
+
+            testNumber++;
+        }
+
+        return factors;
     }
 }
